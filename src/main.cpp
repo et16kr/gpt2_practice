d2b76dc99eceeac4b4d4605e3987f047803186c0
jungkeun kim <et16kr@gmail.com>
2026-03-06 01:07:17 +0900

    add input

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "data_loader.h"
 
 int main() {
-  const char* fname = "/home/et16/aps/inference_practice/gpt2/model.safetensors";
-  DataLoader data_loader(fname);
+  const char* models_file = "/home/et16/aps/inference_practice/gpt2/model.safetensors";
+  const char* inputs_file = "/home/et16/aps/inference_practice/examples/tokenized_cpp/data/gpt2_inputs_20.safetensors";
+  DataLoader models(models_file);
+  DataLoader inputs(inputs_file);
 
   return 0;
 }
